0d6e379a91feaf6be3b6dc50637ced8e604fa36d
nikita <shubin.mykyta@gmail.con>
2018-09-18 23:33:08 +0300

    helloworld app

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello guys. Seems work!</h1>"
+    return "<h1 align="center">Hello all. Seems work!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
